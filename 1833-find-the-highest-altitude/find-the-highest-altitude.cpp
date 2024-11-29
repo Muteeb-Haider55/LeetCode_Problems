7878a119ebab553bbc63db6bfaf9294247f747d7
Muteeb Haider <muteebhdr@gmail.com>
2024-11-29 05:43:21 +0500

    Time: 0 ms (100.00%) | Memory: 10.8 MB (9.54%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maximum=0,maxalt=0;
+    for(int i=0;i<gain.size();++i){
+     maximum+=gain[i];
+     maxalt=max(maximum,maxalt);
+    }
+    return maxalt;
+    }
+};

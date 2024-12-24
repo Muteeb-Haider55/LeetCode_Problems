a7448f7854b069cfe57810403cd0b3a70b33f1d8
Muteeb Haider <b-27240@student.usa.edu.pk>
2024-12-24 21:47:43 +0500

    Format code

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -3,14 +3,10 @@ public:
     int finalValueAfterOperations(vector<string>operations) {
         int X=0;
         for(int i=0;i<operations.size();i++){
-            if(operations[i]=="x++"||operations[i]=="X++")
+            if(operations[i]=="X++"||operations[i]=="++X")
             X++;
-            else if(operations[i]=="++x"||operations[i]=="++X")
-            ++X;
-            else if(operations[i]=="x--"||operations[i]=="--X")
-            X--;
             else
-            --X;
+            X--;
         }
         return X;
     }
